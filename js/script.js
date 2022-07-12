ebe6b40f59255e9082fc35f1058f611fb4bf1af4
tyc007 <tonychan550@gmail.com>
2022-07-11 17:10:32 -0700

    Added random logic to computerPlay

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // Will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function computerPlay(){
     const items = ['Rock', 'Paper', 'Scissors'];
-    console.log(items[1]);
+    console.log(items[getRandomInt(items.length)]);
     return;
 }
+
+// Returns a random number between 0 and max number provided
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+computerPlay();
