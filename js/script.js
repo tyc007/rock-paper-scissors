d1fccb54fcea2c533b7d4dfcc1a29b63b6728c02
tyc007 <tonychan550@gmail.com>
2023-01-30 16:44:23 -0800

    ui and logic changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ['rock', 'paper', 'scissors'];
+
+const textLine = document.getElementById('textLine');
+const playerScoreBoard = document.getElementById('playerScore');
+const computerScoreBoard = document.getElementById('computerScore');
+const buttons = document.querySelectorAll('button');
+
+let playerSelection = '';
+let computerSelection = '';
+let playerScore = 0;
+let computerScore = 0;
 
 // Will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function computerPlay(){
     let computerSelection = choices[getRandomInt(choices.length)];
-    console.log(`Computer chooses ${computerSelection}!`);
+    textLine.textContent = `Computer chooses ${computerSelection}!`;
     return computerSelection;
 }
 
@@ -14,27 +24,40 @@ function getRandomInt(max) {
 
 // Returns the winner between player and computer selection in Rock Paper Scissors game
 function playRound(playerSelection , computerSelection){
-    playerSelection = String(playerSelection).toLowerCase();
-    computerSelection = String(computerSelection).toLowerCase();
+    console.log(playerSelection, computerSelection)
+    if ((playerSelection == 'rock'      && computerSelection == 'scissors')   ||
+        (playerSelection == 'paper'     && computerSelection == 'rock')       ||
+        (playerSelection == 'scissors'  && computerSelection == 'paper')) {
+        textLine.textContent = textLine.textContent + ` You Win! Your ${playerSelection} beats their ${computerSelection}.`;
+        playerScore += 1;
+        playerScoreBoard.textContent = `Player Score: ${playerScore}`
+    }
+    else if(playerSelection == computerSelection){
+        textLine.textContent = textLine.textContent + ` You guys tied.`;
+    }
+    else{
+        textLine.textContent = textLine.textContent + ` You Lose! Their ${computerSelection} beats your ${playerSelection}.`;
+        computerScore += 1;
+        computerScoreBoard.textContent = `Computer Score: ${computerScore}`
+    }
 
-    if ( playerSelection === computerSelection) {
-        return "You guys tied!"
-    }
-    else {
-        if (playerSelection === 'rock'      && computerSelection === 'paper')       return "You Lose! Paper beats Rock";
-        if (playerSelection === 'rock'      && computerSelection === 'scissors')    return "You Win! Rock beats Scissors";
-        if (playerSelection === 'paper'     && computerSelection === 'scissors')    return "You Lose! Scissors beats Paper";
-        if (playerSelection === 'paper'     && computerSelection === 'rock')        return "You Win! Paper beats Rock";
-        if (playerSelection === 'scissors'  && computerSelection === 'rock')        return "You Lose! Rock beats Scissors";
-        if (playerSelection === 'scissors'  && computerSelection === 'paper')       return "You Win! Scissors beats Paper";
-    }
+}
+
+function buttonListeners(){
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.id;
+            computerSelection = computerPlay();
+            playRound(playerSelection, computerSelection);
+        });
+      });
 }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Let's play Rock, Paper Scissors! Put in your choice!");
-        alert(playRound(playerSelection, computerPlay() ) );
-    }
+    //let playerSelection = prompt
+    textLine.textContent = "Let's play Rock, Paper Scissors! Put in your choice!";
+    
 }
 
+buttonListeners();
 game();
